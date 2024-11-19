e9b26c564bc5644b9806918c381a3684d797c9e5
Maurice Maet <maurice.maet@student.howest.be>
2024-11-19 15:53:17 +0100

    Update B2 Medium

--- B2Medium/Program.cs
+++ B2Medium/Program.cs
@@ -9,29 +9,47 @@ class Program
     {
         var client = new HackTheFutureClient();
 
-        await client.Login();
+        try
+        {
+            await client.Login();
 
-        string apiRoute = "/api/b/medium/puzzle";
+            string apiRoute = "/api/b/medium/puzzle";
 
-        string jsonSendDateTime = await client.GetData(apiRoute, "sendDateTime");
-        string jsonTravelSpeed = await client.GetData(apiRoute, "travelSpeed");
-        string jsonDistance = await client.GetData(apiRoute, "distance");
-        string jsonDayLength = await client.GetData(apiRoute, "dayLength");
+            string jsonSendDateTime = await client.GetData(apiRoute, "sendDateTime");
+            string jsonTravelSpeed = await client.GetData(apiRoute, "travelSpeed");
+            string jsonDistance = await client.GetData(apiRoute, "distance");
+            string jsonDayLength = await client.GetData(apiRoute, "dayLength");
 
-        DateTime sendDateTime = ParseDateTime(jsonSendDateTime);
+            DateTime sendDateTime = ParseDateTime(jsonSendDateTime);
+            double travelSpeed = JsonConvert.DeserializeObject<double>(jsonTravelSpeed);
+            double distance = JsonConvert.DeserializeObject<double>(jsonDistance);
+            int dayLengthInHours = JsonConvert.DeserializeObject<int>(jsonDayLength);
 
-        double travelSpeed = JsonConvert.DeserializeObject<double>(jsonTravelSpeed);  
-        double distance = JsonConvert.DeserializeObject<double>(jsonDistance);  
-        int dayLengthInHours = JsonConvert.DeserializeObject<int>(jsonDayLength);  
+            Console.WriteLine($"Send DateTime: {sendDateTime} \n");
+            Console.WriteLine($"Travel Speed: {travelSpeed} light-years/minute \n");
+            Console.WriteLine($"Distance: {distance} light-years \n");
+            Console.WriteLine($"Day Length: {dayLengthInHours} hours \n");
 
-        Console.WriteLine($"Send DateTime: {sendDateTime}");
-        Console.WriteLine($"Travel Speed: {travelSpeed} light-years/minute");
-        Console.WriteLine($"Distance: {distance} light-years");
-        Console.WriteLine($"Day Length: {dayLengthInHours} hours");
+            DateTime arrivalTime = CalculateArrivalTime(sendDateTime, travelSpeed, distance, dayLengthInHours);
 
-        DateTime arrivalTime = CalculateArrivalTime(sendDateTime, travelSpeed, distance, dayLengthInHours);
+            Console.WriteLine($"Arrival time: {arrivalTime.ToString("yyyy-MM-ddTHH:mm:ssZ")}\n");
 
-        Console.WriteLine($"Arrival time: {arrivalTime.ToString("yyyy-MM-ddTHH:mm:ssZ")}");
+            try
+            {
+                string postRoute = "/api/b/medium/puzzle";
+                string payload = arrivalTime.ToString("yyyy-MM-ddTHH:mm:ssZ");
+                var response = await client.PostData(postRoute, payload);
+                Console.WriteLine("Post response: " + await response.Content.ReadAsStringAsync());
+            }
+            catch (Exception postEx)
+            {
+                Console.WriteLine($"Failed to post arrival time: {postEx.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
     }
 
     static DateTime ParseDateTime(string jsonSendDateTime)
@@ -56,7 +74,6 @@ class Program
     static DateTime CalculateArrivalTime(DateTime sendDateTime, double travelSpeed, double distance, int dayLengthInHours)
     {
         double travelTimeInMinutes = (distance / travelSpeed) * 60;
-
         double minutesInADay = dayLengthInHours * 60;
 
         DateTime arrivalTime = sendDateTime.AddMinutes(travelTimeInMinutes);
